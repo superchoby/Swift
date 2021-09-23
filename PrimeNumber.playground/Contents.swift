e01d912cfb7d26f8dae884108f0eddad30c5f0ca
superchoby <thomasqtrnh@gmail.com>
2021-09-23 11:58:58 -0700

    added comments

--- PrimeNumber.playground/Contents.swift
+++ PrimeNumber.playground/Contents.swift
@@ -1,4 +1,24 @@
 import UIKit
+/*
+ 
+ # Is Prime Checker
+ 
+ ## Pseudo Code
+ loop from 2 -> numberInParam - 1
+    if numberInParam divisible by number in range:
+        return false
+ return true
+ 
+ ## Why I Chose This
+ I love math challenges and I felt like this was an exciting problem
+ 
+ ## Paramters and Variables
+ num - The number to check if it is prime
+ testNum - integer from range 2 to to num - 1
+ 
+ ## Variables vs Constants
+ Variables can be changed while constants can't be
+*/
 
 func isPrime(num: Int) -> Bool {
     for testNum in 2...(num - 1) {
--- VerifyParenthesis.playground/Contents.swift
+++ VerifyParenthesis.playground/Contents.swift
@@ -1,5 +1,29 @@
 import UIKit
 
+/*
+ 
+ # VerifyParenthesis
+ 
+ ## Pseudo Code
+ var openBracketCounter = 0
+ for char in parenthesisString {
+    if char is openBracket: openBracketCounter++
+    else if char is closedBracket: return false if openBracketCounter == 0 else openBracketCounter--
+ }
+ 
+ return openBracketCounter == 0
+ 
+ ## Why I Chose This
+ I like creating algorithms to solve issues and I found this one to be challenging and exciting.
+ 
+ ## Paramters and Variables
+ string - The string of parenthesis
+ openBrackets - number of unmatched open brackets
+ 
+ ## Variables vs Constants
+ Variables can be changed while constants can't be
+*/
+
 var str = "Hello, playground"
 func verifyParenthesis(string: String) -> Bool {
     var openBrackets = 0;
